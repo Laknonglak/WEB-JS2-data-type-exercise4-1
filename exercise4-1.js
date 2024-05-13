5c1a2e1411f07c5f0157655760fbe9f906d0cd11
noeysoftware <noeysoftware@gmail.com>
2024-05-13 14:33:09 +0700

    finished exercise4-1

---
+++ exercise4-1.js
@@ -0,0 +1,23 @@
+const Products = {
+    product_id: "ABC123",
+    quantity_in_stock: 50,
+    unit_price: 10.99,
+};
+
+const product_description = {
+    product_brand: "Rilakkuma",
+    size_or_quantity: "Medium",
+    taste_or_scent: "None",
+    colour: "Brown",
+    additional_description: "Adorable teddy bear with a lazy attitude.",
+};
+
+console.log("Product ID:", Products.product_id);
+console.log("Quantity in Stock:", Products.quantity_in_stock);
+console.log("Unit Price:", Products.unit_price);
+
+console.log("Product Brand:", product_description.product_brand);
+console.log("Size or Quantity:", product_description.size_or_quantity);
+console.log("Taste or Scent:", product_description.taste_or_scent);
+console.log("Colour:", product_description.colour);
+console.log("Additional Description:", product_description.additional_description);
